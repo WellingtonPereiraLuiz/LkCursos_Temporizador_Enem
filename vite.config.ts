753f9ed136edb659c9e3fcafe129bf870e54cded
wellington pereira luiz <108313369+WellingtonPereiraLuiz@users.noreply.github.com>
2025-05-14 16:54:50 -0400

    Atualizações no projeto: ajustes de layout e config para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/LkCursos_Temporizador_Enem",
+  base: '/LkCursos_Temporizador_Enem/',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
