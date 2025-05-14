b34174183981b10477fe1504654603a282a1bf41
wellington pereira luiz <108313369+WellingtonPereiraLuiz@users.noreply.github.com>
2025-05-14 16:26:43 -0400

    configuracoes de deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/LkCursos_Temporizador_Enem/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
